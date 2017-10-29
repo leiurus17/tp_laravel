bb537bf8983cc77bf475d23efd65ca36c20a916a
Marlon <martianworm17@gmail.com>
2017-10-29 12:14:52 +0800

    Edit Welcome page. Add Kamote Labs

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Kamote Labs
                 </div>
 
                 <div class="links">
