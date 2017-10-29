d42703a40029f26a2418d9000b7e69744443a42c
Marlon <martianworm17@gmail.com>
2017-10-29 14:37:04 +0800

    Add a route to a kamote.blade.php file

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
+
+Route::get('/kamote', function () {
+    return view('kamote');
+});
