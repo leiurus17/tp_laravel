66352914e2d28ab301d84d4a52ae2cb8054c279e
Marlon <martianworm17@gmail.com>
2017-10-29 14:47:46 +0800

    Routing Parameters - Optional Parameters

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -24,5 +24,10 @@ Route::get('/kamote', function () {
 });
 
 Route::get('ID/{id}', function ($id){
-    echo 'ID: '.$id;
+    echo 'ID: ' .$id;
 });
+
+Route::get('user/{name?}', function ($name = 'Uzumaki Naruto'){
+    echo 'Name: ' .$name;
+});
+        
